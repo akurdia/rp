052538c9e207f1bfe559d839f823ce307c3623f0
Anastasia Kurdia <akurdia@tulane.edu>
2021-11-02 03:02:00 +0000

    google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
  OmniAuth.config.allowed_request_methods = [:post, :get]
-  provider :google,  "936217471773-52eus5oa5bpnrj800he8ulefv0ej8lq0.apps.googleusercontent.com", "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R"
+  provider :google,  "936217471773-52eus5oa5bpnrj800he8ulefv0ej8lq0.apps.googleusercontent.com", "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"
 end
